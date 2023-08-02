48408d0c2ec17c7d74772687c98f1d1233c58c01
fans <94341379+marsfans@users.noreply.github.com>
2023-08-02 20:05:16 +0800

    update

--- start1.sh
+++ start1.sh
@@ -1,5 +1,5 @@
-wget https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.15.4/shadowsocks-v1.15.4.x86_64-unknown-linux-musl.tar.xz
-tar xf ./shadowsocks-v1.15.4.x86_64-unknown-linux-musl.tar.xz
+wget https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.15.4/shadowsocks-v1.15.4.x86_64-unknown-linux-gnu.tar.xz
+tar xf ./shadowsocks-v1.15.4.x86_64-unknown-linux-gnu.tar.xz
 chmod +x ./ssserver 
 chmod +x ./sslocal
 chmod +x ./ssmanager
