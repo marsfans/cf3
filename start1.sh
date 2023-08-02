814e5a16a8e26f4897791255ff78c7f19d44d12c
fans <94341379+marsfans@users.noreply.github.com>
2023-08-02 20:56:03 +0800

    update

--- start1.sh
+++ start1.sh
@@ -1,2 +1,2 @@
 # ./ssserver -c ./config-sh.json
-./shadowsocks2-linux -s 'ss://AEAD_CHACHA20_POLY1305:134679@:8388' -verbose
+./shadowsocks2-linux -s 'ss://AEAD_CHACHA20_POLY1305:134679@0.0.0.0:8388' -verbose
